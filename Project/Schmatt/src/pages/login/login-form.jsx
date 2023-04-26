b69009950072f6e4cdb69c3a6696ffc5705d85bb
mpu69 <elura001@osloskolen.no>
2023-04-26 14:39:05 +0200

    updated login page

--- Project/Schmatt/src/pages/login/login-form.jsx
+++ Project/Schmatt/src/pages/login/login-form.jsx
@@ -1,8 +1,16 @@
 export default function LoginForm() {
   return (
-    <div className="h-96 w-3/5 bg-white rounded-3xl flex overflow-hidden">
-      <div className="bg-red-400 w-1/3 h-full"></div>
-      <div className="bg-blue-400 w-2/3 h-full"></div>
+    <div className="h-3/6 w-3/5 bg-white rounded-3xl flex flex-col overflow-hidden">
+      <div className="h-96 flex">
+        <div className="bg-red-400 h-full">
+          <img src={"mobile.png"} alt="mobile image" className="h-full" />
+        </div>
+        <div className="bg-red-400 w-2/3 h-full">
+          <h1></h1>
+          <div></div>
+          <button></button>
+        </div>
+      </div>
     </div>
   );
 }
