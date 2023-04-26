2471a9cd6b15947f28f68434bc5d992a018870b1
korneliushen <korneliussen06@gmail.com>
2023-04-26 13:56:46 +0200

    Satte opp firebase

---
+++ Project/Schmatt/src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhop4GpoqB4IiziyBV5ZbXwlpjODOOo2U",
+  authDomain: "schmatt-5ea5a.firebaseapp.com",
+  projectId: "schmatt-5ea5a",
+  storageBucket: "schmatt-5ea5a.appspot.com",
+  messagingSenderId: "802169341254",
+  appId: "1:802169341254:web:f04fe9898edfdfc1fcd147",
+};
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
