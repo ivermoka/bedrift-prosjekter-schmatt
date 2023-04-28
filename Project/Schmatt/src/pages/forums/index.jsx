d2ccda685fb63062f80aca0e4125e6ec22da853e
Max Petzold <maxpetzold581@gmail.com>
2023-04-28 11:44:01 +0200

    navbar

--- Project/Schmatt/src/pages/forums/index.jsx
+++ Project/Schmatt/src/pages/forums/index.jsx
@@ -1,21 +1,12 @@
-<<<<<<< Updated upstream
-import Navbar from "../navbar"
+import Navbar from "./../navbar/navbar"
 
 export default function f() {
 
-=======
-import Navbar from "../navbar/navbar"
->>>>>>> Stashed changes
 
-export default function MyComponent() {
     return (
         <>
             <Navbar />
+
         </>
     );
 }
-
-
-
-
-
