734783317a4b99db26e2211985b5371d8832ea73
mpu69 <elura001@osloskolen.no>
2023-04-28 15:04:41 +0200

    la til timestamp på messages

--- Project/Schmatt/src/pages/chat/components/Message.jsx
+++ Project/Schmatt/src/pages/chat/components/Message.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import {auth} from '../../../firebase-config'
-import { Timestamp } from 'firebase/firestore'
-
+import React from "react";
+import { auth } from "../../../firebase-config";
+import { Timestamp } from "firebase/firestore";
 
 const style = {
   message: `flex items-center drop-shadow-[4xl] m-4 py-2 px-3 rounded-tl-full rounded-tr-full cursor-pointer hover:bg-gray-800 hover:text-gray-8`,
   name: `mt-[-4rem] text-gray-600 text-xs`,
   sent: `bg-message-sent text-text-color flex-row-reverse text-end float-right rounded-bl-full`,
   received: `bg-message-recieved text-text-color flex-row-reverse text-end float-left rounded-br-full`,
-}
+};
 const Message = ({ message }) => {
   const messageClass =
-  message.uid === auth.currentUser.uid
-  ?`${style.sent}`
-  :`${style.received}`
+    message.uid === auth.currentUser.uid
+      ? `${style.sent}`
+      : `${style.received}`;
+
+  const timestamp = new Date(message.timestamp.seconds * 1000);
+
+  const timestampString = `${timestamp.toLocaleDateString()} ${timestamp.toLocaleTimeString()}`;
 
   return (
     <div>
-        <div className={`${style.message} ${messageClass}`}>
-          <p className={style.name}>{message.name}</p>
-          <p>{message.text}</p>
-        </div>
+      <div className={`${style.message} ${messageClass}`}>
+        <p className={style.name}>
+          {message.name} {timestampString}
+        </p>
+        <p>{message.text}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Message;
--- Project/Schmatt/src/pages/chat/components/RoomButton.jsx
+++ Project/Schmatt/src/pages/chat/components/RoomButton.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const RoomButton = () => {
-    const [button, setButton] = useState(true);
+  const [button, setButton] = useState(true);
 
-    function changeButton() {
-        setButton(button =>!button)
-    }
+  function changeButton() {
+    setButton((button) => !button);
+  }
 
-
-
-    const buttonClass = button ? 'bg-common text-white scale-x-100 duration-75' : ' scale-x-[109%] translate-x-4 duration-75 p-5 rounded-sm';
+  const buttonClass = button
+    ? "bg-common text-white scale-x-100 duration-75"
+    : " scale-x-[109%] translate-x-4 duration-75 p-5 rounded-sm";
   return (
-    <button id='room-button' onClick={changeButton} className={`  relative border-[1px] border-border-color w-[90%] p-5 bg-button-active rounded-sm ${buttonClass}`}>
-        <p></p>
+    <button
+      id="room-button"
+      onClick={changeButton}
+      className={`  relative border-[1px] border-border-color w-[90%] p-5 bg-button-active rounded-sm ${buttonClass}`}
+    >
+      <p></p>
     </button>
-  )
-}
+  );
+};
 
-export default RoomButton
+export default RoomButton;
