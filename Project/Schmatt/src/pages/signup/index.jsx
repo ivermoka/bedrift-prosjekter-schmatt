9f734ec4d4239c25e0d29ae68ac7767c0b96cdd5
mpu69 <elura001@osloskolen.no>
2023-04-28 12:32:39 +0200

    login page ferdig, layout for sign up page ferdig

---
+++ Project/Schmatt/src/pages/signup/index.jsx
@@ -0,0 +1,9 @@
+import Form from "./signup-form";
+
+export default function Login() {
+  return (
+    <div className="h-screen bg-rich-black flex justify-center items-center">
+      <Form />
+    </div>
+  );
+}
