ad4a127f2f342d3071f413fd3ddcc49ff5e9a64c
ivermoka <ivermoka@icloud.com>
2023-04-28 14:22:14 +0200

    forbedret chat

--- Project/Schmatt/src/pages/navbarfolder/navbar.jsx
+++ Project/Schmatt/src/pages/navbarfolder/navbar.jsx
@@ -4,9 +4,9 @@ import Profile from "./components/profile"
 export default function Navbar() {
     return (
         <>
-            <nav className=' flex flex-row w-screen  h-24'>
+            <nav className=' flex flex-row w-screen h-14  bg-rich-black'>
                 <div id='left' className=' flex w-3/5 '>
-                    <div className='w-[100px]'>  <a href="/"><img className=' mt-2 h-auto w-full ml-20 ' src={"temp-logo-schmatt.png"} /></a> </div>
+                    <div className='w-[100px]'>  <a href="/"><img className=' mt-2 h-auto w-2/3 ml-20 ' src={"temp-logo-schmatt.png"} /></a> </div>
                 </div>
                 <div id='right' className='w-2/5 flex flex-row justify-start   gap-28 items-center'>
                     <Searchbar />
