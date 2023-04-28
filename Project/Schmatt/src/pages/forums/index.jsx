ec0947d51e0eaedc794f6883e464e1d750bed3e1
Max Petzold <maxpetzold581@gmail.com>
2023-04-28 11:39:29 +0200

    Begynte på navbar

--- Project/Schmatt/src/pages/forums/index.jsx
+++ Project/Schmatt/src/pages/forums/index.jsx
@@ -1,12 +1,21 @@
+<<<<<<< Updated upstream
 import Navbar from "../navbar"
 
 export default function f() {
 
+=======
+import Navbar from "../navbar/navbar"
+>>>>>>> Stashed changes
 
+export default function MyComponent() {
     return (
         <>
             <Navbar />
-
         </>
     );
 }
+
+
+
+
+
---
+++ Project/Schmatt/src/pages/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Searchbar from "./searchbar"
+export default function Navbar() {
+    return (
+        <>
+            <nav className=' flex flex-row h-20'>
+                <div id='left' className=' flex w-3/5 bg-gray-900 '>
+                    <div className='w-[100px]'>  <img className=' h-full w-full ml-20 ' src={"ailogofortesting.webp"} /></div>
+                </div>
+                <div id='right' className='w-2/5 bg-gray-900 flex flex-row justify-start gap-12 items-center'>
+                    <Searchbar />
+                </div>
+            </nav>
+
+        </>)
+}
+
+
