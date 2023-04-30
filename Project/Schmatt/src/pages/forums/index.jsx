8b77c6aff69df0d600f01efc42b49dbd0d013c8c
Max Petzold <maxpetzold581@gmail.com>
2023-04-30 13:10:45 +0200

    .

--- Project/Schmatt/src/pages/forums/index.jsx
+++ Project/Schmatt/src/pages/forums/index.jsx
@@ -1,12 +1,18 @@
 import Navbar from "./../navbarfolder/navbar"
+import Forumparent from "./components/forumselection"
+
+
 
 export default function forums() {
 
 
     return (
         <>
-            <main >
+            <main className="w-screen" >
                 <Navbar />
+                <section className="flex items-center">
+                    <Forumparent />
+                </section>
             </main>
         </>
     );
