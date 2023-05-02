adebba5dd5f457703a29c3e0ea183ff060f3da4d
ivermoka <ivermoka@icloud.com>
2023-05-02 14:44:39 +0200

    bit change

--- Project/Schmatt/src/pages/chat/components/Rooms.jsx
+++ Project/Schmatt/src/pages/chat/components/Rooms.jsx
@@ -24,10 +24,12 @@ const Rooms = () => {
         <input className='bg-common' type='text' id="new-room-name" value={newRoomName} onChange={(e) => setNewRoomName(e.target.value)} placeholder='New room...' />
         <button className=' bg-button-active w-1/3 rounded-md ' type='submit'>Create</button>
       </form>
-      <form className=' bg-common py-4 px-2 rounded-lg border-border-color border-[1px] mt-1 mb-5'>
+
+      {/* add back when we create private rooms */}
+      {/* <form className=' bg-common py-4 px-2 rounded-lg border-border-color border-[1px] mt-1 mb-5'>
         <input className='bg-common' type='text' placeholder='New person...' />
         <button className=' bg-button-active w-1/3 rounded-md ' type='submit'>Add</button>
-      </form>
+      </form> */}
 
       {/* rooms */}
       {rooms.map((roomName) => (
