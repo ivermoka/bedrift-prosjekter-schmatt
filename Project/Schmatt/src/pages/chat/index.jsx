53f376a1aaae8bc744aff94020fe01bf22501078
ivermoka <ivermoka@icloud.com>
2023-05-02 12:59:18 +0200

    div changes

--- Project/Schmatt/src/pages/chat/index.jsx
+++ Project/Schmatt/src/pages/chat/index.jsx
@@ -6,7 +6,7 @@ import Chat from './components/Chat'
 import { useState, useEffect } from 'react'
 import { db } from '../../firebase-config';
 import {query, collection, orderBy, onSnapshot} from "firebase/firestore" ; 
-import Navbar from '../navbarfolder/navbar'
+import Navbar from '../navbar/navbar'
 import UsernameList from './components/UsernameList'
 
 
@@ -32,7 +32,7 @@ const index = () => {
     <>
       <div className='overflow-hidden max-h-screen'>
         <Navbar />
-        <div className=' w-screen h-screen bg-rich-black flex flex-row'>
+        <div className='mt-14 w-screen h-screen bg-rich-black flex flex-row'>
           
           <Rooms />
           <Chat />
