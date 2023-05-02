1dd4eded254e4b6c7fe537abcdd5eaa655e34211
korneliushen <korneliussen06@gmail.com>
2023-05-02 14:27:40 +0200

    Halvveis fikset brukernavn over chat

--- Project/Schmatt/src/pages/chat/components/Chat.jsx
+++ Project/Schmatt/src/pages/chat/components/Chat.jsx
@@ -1,43 +1,40 @@
-import SendMessage from './SendMessage';
-import Message from './Message';
-import React, { useRef } from 'react';
-import { useState, useEffect } from 'react'
-import { db } from '../../../firebase-config';
-import {query, collection, orderBy, onSnapshot} from "firebase/firestore" ; 
-
-
+import SendMessage from "./SendMessage";
+import Message from "./Message";
+import React, { useRef } from "react";
+import { useState, useEffect } from "react";
+import { db } from "../../../firebase-config";
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
   const scroll = useRef();
-  
+
   useEffect(() => {
-    const q = query(collection(db, 'messages'), orderBy('timestamp'));
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let messages = [];
       querySnapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
       setMessages(messages);
-      console.log(messages)
+      console.log(messages);
     });
     return () => unsubscribe();
   }, []);
   return (
     <>
-        <div className=' overflow-scroll w-[65%] border-r-[1px] border-border-color min-h-full flex flex-col pb-12 pt-10'>
-            
+      <div className=" overflow-scroll w-[65%] border-r-[1px] border-border-color min-h-full flex flex-col pb-12 pt-10">
+        <div className=" pb-20">
           {messages.map((message) => (
-              <Message key={message.id} message={message} />
-            ))}
-          
-          
-          <SendMessage scroll={scroll} />
-          <span ref={scroll}></span>
-          
+            <Message key={message.id} message={message} />
+          ))}
         </div>
+
+        <SendMessage scroll={scroll} />
+        <span ref={scroll}></span>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Chat;
--- Project/Schmatt/src/pages/chat/components/Message.jsx
+++ Project/Schmatt/src/pages/chat/components/Message.jsx
@@ -3,10 +3,12 @@ import { auth } from "../../../firebase-config";
 import { Timestamp } from "firebase/firestore";
 
 const style = {
-  message: `flex items-center drop-shadow-[4xl] m-4 py-2 px-3 rounded-tl-full rounded-tr-full cursor-pointer hover:bg-gray-800 hover:text-gray-8`,
-  name: `mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-message-sent text-text-color flex-row-reverse text-end float-right rounded-bl-full`,
-  received: `bg-message-recieved text-text-color flex-row-reverse text-end float-left rounded-br-full`,
+  message: `flex items-center drop-shadow-[4xl] mx-3 mb-3 py-2 px-3 rounded-tl-full rounded-tr-full cursor-pointer hover:bg-gray-800 hover:text-gray-8`,
+  name: `text-gray-600 text-xs pb-[1px]`,
+  sent: `bg-message-sent text-text-color flex-col flex-col text-end float-right rounded-bl-full`,
+  sentText: `text-text-color flex-col pr-5 flex-col text-end float-right rounded-bl-full`,
+  received: `bg-message-recieved  text-text-color flex-col flex-col text-end float-left rounded-br-full`,
+  receivedText: `text-text-color pl-5 flex-col flex-col text-start float-left rounded-br-full`,
 };
 const Message = ({ message }) => {
   const messageClass =
@@ -14,11 +16,20 @@ const Message = ({ message }) => {
       ? `${style.sent}`
       : `${style.received}`;
 
+  const messageText =
+    message.uid === auth.currentUser.uid
+      ? `${style.sentText}`
+      : `${style.receivedText}`;
+
   return (
     <div>
-      <div className={`${style.message} ${messageClass}`}>
-        <p className={style.name}>{message.name}</p>
-        <p>{message.text}</p>
+      <div className="w-full h-20">
+        <div className={`${messageText}`}>
+          <p className={style.name}>{message.name}</p>
+          <div className={`${style.message} ${messageClass}`}>
+            <p>{message.text}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
