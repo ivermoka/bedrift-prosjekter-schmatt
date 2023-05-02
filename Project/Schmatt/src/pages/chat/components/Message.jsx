21300420a15634b9e900d8aaf1f94b66b3f5335c
korneliushen <korneliussen06@gmail.com>
2023-05-02 15:14:59 +0200

    La til timestamps

--- Project/Schmatt/src/pages/chat/components/Message.jsx
+++ Project/Schmatt/src/pages/chat/components/Message.jsx
@@ -5,28 +5,35 @@ import { Timestamp } from "firebase/firestore";
 const style = {
   message: `flex items-center drop-shadow-[4xl] mx-3 mb-3 py-2 px-3 rounded-tl-full rounded-tr-full cursor-pointer hover:bg-gray-800 hover:text-gray-8`,
   name: `text-gray-600 text-xs pb-[1px]`,
-  sent: `bg-message-sent text-text-color flex-col flex-col text-end float-right rounded-bl-full`,
-  sentText: `text-text-color flex-col pr-5 flex-col text-end float-right rounded-bl-full`,
-  received: `bg-message-recieved  text-text-color flex-col flex-col text-end float-left rounded-br-full`,
-  receivedText: `text-text-color pl-5 flex-col flex-col text-start float-left rounded-br-full`,
+  sent: `bg-message-sent text-text-color flex-col text-end rounded-bl-full`,
+  sentText: `text-text-color flex-col pr-5 text-end rounded-bl-full justify-self-end`,
+  received: `bg-message-recieved  text-text-color flex-col text-start rounded-br-full`,
+  receivedText: `text-text-color pl-5 flex-col text-start rounded-br-full justify-self-start`,
+  timestamp: `text-gray-500 text-xs float-right`,
 };
+
 const Message = ({ message }) => {
   const messageClass =
     message.uid === auth.currentUser.uid
-      ? `${style.sent}`
+      ? `${style.sent} `
       : `${style.received}`;
 
   const messageText =
     message.uid === auth.currentUser.uid
       ? `${style.sentText}`
       : `${style.receivedText}`;
-
+  const timestamp = new Timestamp(
+    message.timestamp.seconds,
+    message.timestamp.nanoseconds
+  ).toDate();
+  const formattedTime = `${timestamp.getHours()}:${timestamp.getMinutes()} ${timestamp.toDateString()}`;
   return (
     <div>
-      <div className="w-full h-20">
-        <div className={`${messageText}`}>
+      <div className="w-full h-20 grid">
+        <div className={`flex ${messageText}`}>
           <p className={style.name}>{message.name}</p>
           <div className={`${style.message} ${messageClass}`}>
+            <p className={style.timestamp}>{formattedTime}</p>
             <p>{message.text}</p>
           </div>
         </div>
