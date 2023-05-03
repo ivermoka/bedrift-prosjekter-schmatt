3c7a420e35425233a8983ea0377d4b3317d90f57
Max Petzold <maxpetzold581@gmail.com>
2023-05-03 13:50:28 +0200

    forumsdisclosuremenu

--- Project/Schmatt/src/pages/forums/components/disclosure-menu.jsx
+++ Project/Schmatt/src/pages/forums/components/disclosure-menu.jsx
@@ -1,7 +1,7 @@
 import { Disclosure } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
-export default function Example() {
+export default function Discmenu() {
     return (
         <div className="w-full px-4 pt-16">
             <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
@@ -9,15 +9,23 @@ export default function Example() {
                     {({ open }) => (
                         <>
                             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                                <span>What is your refund policy?</span>
+                                <span>Coding</span>
                                 <ChevronUpIcon
                                     className={`${open ? 'rotate-180 transform' : ''
                                         } h-5 w-5 text-purple-500`}
                                 />
                             </Disclosure.Button>
                             <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                                If you're unhappy with your purchase for any reason, email us
-                                within 90 days and we'll refund you in full, no questions asked.
+                                <a href="/"> s/CodingTips</a>
+                            </Disclosure.Panel>
+                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                s/CodingNews
+                            </Disclosure.Panel><Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                s/Webdevelopment
+                            </Disclosure.Panel><Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                s/CodingHelp
+                            </Disclosure.Panel><Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                                s/CodingTips
                             </Disclosure.Panel>
                         </>
                     )}
@@ -34,6 +42,22 @@ export default function Example() {
                             </Disclosure.Button>
                             <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
                                 No.
+                            </Disclosure.Panel>
+                        </>
+                    )}
+                </Disclosure>
+                <Disclosure as="div" className="mt-2">
+                    {({ open }) => (
+                        <>
+                            <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                <span>bruh</span>
+                                <ChevronUpIcon
+                                    className={`${open ? 'rotate-180 transform' : ''
+                                        } h-5 w-5 text-purple-500`}
+                                />
+                            </Disclosure.Button>
+                            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+
                             </Disclosure.Panel>
                         </>
                     )}
