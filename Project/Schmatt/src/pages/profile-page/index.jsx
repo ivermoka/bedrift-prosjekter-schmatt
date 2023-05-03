aa851e4c2c0866c724f130647b827c6ca7568b24
mpu69 <elura001@osloskolen.no>
2023-05-03 10:35:30 +0200

    cum

--- Project/Schmatt/src/pages/profile-page/index.jsx
+++ Project/Schmatt/src/pages/profile-page/index.jsx
@@ -1,3 +1,29 @@
+import { auth } from "./../../firebase-config";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
+
 export default function ProfilePage() {
-  return <div className="w-screen h-screen bg-red-400"></div>;
+  const [user, setUser] = useState({});
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+  return (
+    <div className="w-screen h-screen bg-rich-black flex justify-center items-center">
+      <div className="bg-button-active w-1/3 h-5/6 flex flex-col p-10">
+        {/*Image and username*/}
+        <div className="h-40 w-full flex items-end">
+          <div className="self-center w-1/2 aspect-square flex flex-col ">
+            <img
+              className="aspect-square w-full self-center"
+              src={"userIcon_placeholder.png"}
+            />
+            <button className="border border-border-color bg-rich-black rounded-md">
+              Change picture
+            </button>
+          </div>
+          <h1 className="mb-6 text-7xl text-text-color">{user.displayName}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
