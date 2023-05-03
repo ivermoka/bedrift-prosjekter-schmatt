25740bbc606b49f740ea48a634eb471de782f153
mpu69 <elura001@osloskolen.no>
2023-05-03 11:00:59 +0200

    FIKSA DROPDOWN BUG

--- Project/Schmatt/src/pages/profile-page/index.jsx
+++ Project/Schmatt/src/pages/profile-page/index.jsx
@@ -1,25 +1,42 @@
-import { auth } from "./../../firebase-config";
-import { onAuthStateChanged } from "firebase/auth";
 import { useState } from "react";
+import { auth, storage } from "./../../firebase-config";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function ProfilePage() {
   const [user, setUser] = useState({});
+
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
   });
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const storageRef = ref(storage, `users/${user.uid}/profilePicture`);
+    uploadBytes(storageRef, file).then(() => {
+      getDownloadURL(storageRef).then((url) => {
+        setUser((prevUser) => ({
+          ...prevUser,
+          photoURL: url,
+        }));
+      });
+    });
+  };
+
   return (
     <div className="w-screen h-screen bg-rich-black flex justify-center items-center">
       <div className="bg-button-active w-1/3 h-5/6 flex flex-col p-10">
         {/*Image and username*/}
         <div className="h-40 w-full flex items-end">
-          <div className="self-center w-1/2 aspect-square flex flex-col ">
+          <div className="self-center w-1/2 aspect-square flex flex-col">
             <img
               className="aspect-square w-full self-center"
-              src={"userIcon_placeholder.png"}
+              src={user.photoURL}
             />
-            <button className="border border-border-color bg-rich-black rounded-md">
+            <label className="border border-border-color bg-rich-black rounded-md">
+              <input type="file" onChange={handleImageUpload} hidden />
               Change picture
-            </button>
+            </label>
           </div>
           <h1 className="mb-6 text-7xl text-text-color">{user.displayName}</h1>
         </div>
