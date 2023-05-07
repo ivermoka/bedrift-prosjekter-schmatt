9cdc7fe141ad5c7dd2765175daf92a05a2750acd
ivermoka <119415554+ivermoka@users.noreply.github.com>
2023-05-07 11:30:10 +0200

    home changes
    (getstartedpage)

--- Project/Schmatt/src/pages/home/components/AdPage.jsx
+++ Project/Schmatt/src/pages/home/components/AdPage.jsx
@@ -6,7 +6,7 @@ const AdPage = () => {
         <div className=' w-1/3 h-2/3 border-border-color border'>
           (gif of chat/forums)
         </div>
-        <div className=' w-1/3 pl-10  lg:text-[4vh] md:text-[3vh] sm:text-[2vh] pt-10 h-2/3 text-[2vh] text-border-color  border-border-color border'>
+        <div className=' w-1/3 pl-10  lg:text-[2.5rem] md:text-[1rem] sm:text-[0.5rem] pt-10 h-2/3 text-[1rem] text-border-color  border-border-color border'>
             <p>
                 Introducing our innovative and user-friendly platform 
                 that seamlessly connects you to the world of digital content and creativity.
