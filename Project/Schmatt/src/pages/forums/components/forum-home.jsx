c8c3f4b257e82e3537eb8b2687e064e0f694b37a
Max Petzold <maxpetzold581@gmail.com>
2023-05-08 10:00:24 +0200

    forums

--- Project/Schmatt/src/pages/forums/components/forum-home.jsx
+++
@@ -1,9 +0,0 @@
-export default function forum_home() {
-    <>
-        <main className="flex flex-col h-full gap-24 items-center justify-center">
-            <div>
-                <h1>Forums</h1>
-            </div>
-        </main>
-    </>
-}
