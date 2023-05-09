20e21267e63d0e359236b1587697324d36ef2693
ivermoka <ivermoka@icloud.com>
2023-05-09 10:29:17 +0200

    Update posts.jsx

--- Project/Schmatt/src/pages/forums/components/posts.jsx
+++ Project/Schmatt/src/pages/forums/components/posts.jsx
@@ -11,43 +11,8 @@ const style = {
 };
 export default function Posts() {
   return (
-    <form>
-      <input className={style.input} type="text" placeholder="Your text..." />
-      <button className={style.button} type="submit">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="33.621"
-          height="33.621"
-          viewBox="0 0 33.621 33.621"
-        >
-          <g
-            id="Icon_feather-send"
-            data-name="Icon feather-send"
-            transform="translate(-1.5 -0.879)"
-          >
-            <path
-              id="Path_28"
-              data-name="Path 28"
-              d="M33,3,16.5,19.5"
-              fill="none"
-              stroke="#707070"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="3"
-            />
-            <path
-              id="Path_29"
-              data-name="Path 29"
-              d="M33,3,22.5,33l-6-13.5L3,13.5Z"
-              fill="none"
-              stroke="#707070"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="3"
-            />
-          </g>
-        </svg>
-      </button>
-    </form>
+    <>
+      
+    </>
   );
 }
