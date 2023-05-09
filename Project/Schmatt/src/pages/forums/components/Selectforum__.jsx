4891bc9a3246ac9b361604f47034ff3f2d812faa
ivermoka <ivermoka@icloud.com>
2023-05-09 12:37:42 +0200

    FIKSA HJERNESKADA MT!!

--- Project/Schmatt/src/pages/forums/components/Selectforum__.jsx
+++ Project/Schmatt/src/pages/forums/components/Selectforum__.jsx
@@ -4,7 +4,7 @@ import Discmenu from "./disclosure-menu"
 export default function Selectforum() {
     return (
         <>
-            <div className=" flex flex-col items-center gap-3 w-80 h-3/4  border-gray-700 border-r-[1px] p-4 mt-36">
+            <div className=" flex flex-col items-center gap-3 w-80 h-3/4  border-gray-700 border-r-[1px] p-4">
                 <Forumsearchbar />
                 <Discmenu />
             </div>
