04fbfe30e79aba39ebb793119a540de1fabd1114
korneliushen <korneliussen06@gmail.com>
2023-05-09 10:10:50 +0200

    undone forums

---
+++ Project/Schmatt/src/pages/forums/components/posts.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import { auth, db } from "../../../firebase-config";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import Message from "@/pages/chat/components/MessageSchmatt";
+
+const style = {
+  form: `backdrop-blur-sm pt-[1px] h-14 w-[63%] flex text-xl absolute bottom-0 m-2 justify-between flex-row`,
+  input: `w-[93%] backdrop-blur-sm opacity-75 pl-5 rounded-full text-l text-common p-3 bg-button-active text-white outline-none border-none`,
+  button: `p-3 bg-button-active rounded-full opacity-75`,
+};
+export default function Posts() {
+  return (
+    <form>
+      <input className={style.input} type="text" placeholder="Your text..." />
+      <button className={style.button} type="submit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="33.621"
+          height="33.621"
+          viewBox="0 0 33.621 33.621"
+        >
+          <g
+            id="Icon_feather-send"
+            data-name="Icon feather-send"
+            transform="translate(-1.5 -0.879)"
+          >
+            <path
+              id="Path_28"
+              data-name="Path 28"
+              d="M33,3,16.5,19.5"
+              fill="none"
+              stroke="#707070"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
+            />
+            <path
+              id="Path_29"
+              data-name="Path 29"
+              d="M33,3,22.5,33l-6-13.5L3,13.5Z"
+              fill="none"
+              stroke="#707070"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
+            />
+          </g>
+        </svg>
+      </button>
+    </form>
+  );
+}
--- Project/Schmatt/src/pages/forums/index.jsx
+++ Project/Schmatt/src/pages/forums/index.jsx
@@ -1,22 +1,21 @@
-import Navbar from "../navbar/navbar"
-import Selectforum from "./components/Selectforum__"
-
+import Navbar from "../navbar/navbar";
+import Selectforum from "./components/Selectforum__";
+import Posts from "./components/posts";
 
 export default function forums() {
-    return (
-        <>
-            <Navbar />
-            <main className=" w-screen h-screen bg-[#0F151A] flex items-center" >
-                <div className="flex flex-row items-center w-screen h-4/5 gap-6">
-                    <section id="forumview" className="flex flex-col h-full ">
-                        <Selectforum />
-
-                    </section>
-                    <section className="flex flex-col  ">
-
-                    </section>
-                </div>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <main className=" w-screen h-screen bg-[#0F151A] flex items-center">
+        <div className="flex flex-row items-center w-screen h-4/5 gap-6">
+          <section id="forumview" className="flex flex-col h-full ">
+            <Selectforum />
+          </section>
+          <section className="flex flex-col  ">
+            <Posts />
+          </section>
+        </div>
+      </main>
+    </>
+  );
 }
