6ac79cb0e4935cde6d466c5a75dc0d19ea71f2a4
ivermoka <ivermoka@icloud.com>
2023-05-10 15:00:09 +0200

    postscore update
    with one bug - will probaly change later

--- Project/Schmatt/src/pages/forums/components/ForumPost.jsx
+++ Project/Schmatt/src/pages/forums/components/ForumPost.jsx
@@ -1,29 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import getUser from '@/userSchmatt'
 import { doc, updateDoc, increment } from "firebase/firestore";
 import { db } from '@/firebase-configSchmatt';
+import { Timestamp } from "firebase/firestore";
 
 const ForumPost = ({key, message, selectedForum, title, content}) => {
   const user = getUser()
   const postScore = doc(db, "forum-posts", message.id)
+  const [clickedUpPreviously, setClickedUpPreviously] = useState(false)
+  const [clickedDownPreviously, setClickedDownPreviously] = useState(false)
 
   async function addScore(){
-    await updateDoc(postScore, {
-      score: increment(1)
-    });
+    if (clickedUpPreviously) {
+      await updateDoc(postScore, {
+        score: increment(-1)
+      });
+      setClickedUpPreviously(false);
+      setClickedDownPreviously(false);
+      console.log("clickedUpDown")
+      return;
+      
+    }
+    else {
+      await updateDoc(postScore, {
+        score: increment(1)
+      });
+      setClickedUpPreviously(true);
+      setClickedDownPreviously(false);
+      console.log("clickedUp")
+    }
   }
   async function subtractScore(){
-    await updateDoc(postScore, {
-      score: increment(-1)
-    });
+    if (clickedDownPreviously) {
+      await updateDoc(postScore, {
+        score: increment(1)
+      });
+      setClickedUpPreviously(false);
+      setClickedDownPreviously(false);
+      console.log("clickedDownUp")
+      return;
+      
+    }
+    else {
+      await updateDoc(postScore, {
+        score: increment(-1)
+      });
+      setClickedUpPreviously(false);
+      setClickedDownPreviously(true);
+      console.log("clickedDown")
+    }
   }
+  const formattedTime = message.timestamp
+    ? new Timestamp(
+        message.timestamp.seconds,
+        message.timestamp.nanoseconds
+      ).toDate()
+    : null;
+  let timestampString = formattedTime
+    ? `${formattedTime.getHours()}:${formattedTime.getMinutes()} ${formattedTime.toDateString()}`
+    : "";
 
   return (
     <div>
         <div className=" px-2 w-full h-auto bg-forum-background border-border-color border rounded-md flex flex-col gap-4">
             <div className='flex justify-between h-10 w-full'>
                 <p>{selectedForum}</p>
-                <p>Posted by {message.name}</p>
+                <div className='w-1/4'>
+                  <p>Posted by {message.name}</p>
+                  <p>{timestampString}</p>
+                </div>
+                
             </div>
             <div className='flex flex-col items-center gap-10 px-24 pb-10'>
                 {/* <p >{message.name}</p> */}
@@ -35,11 +81,11 @@ const ForumPost = ({key, message, selectedForum, title, content}) => {
             </div>
             <div className='flex justify-evenly h-10 w-full'>
                 <div className='h-1/4 w-1/4 flex flex-row'>
-                  <svg onClick={addScore} fill="#ffffff" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.997 13.374l-7.081 7.081L7 18.54l8.997-8.998 9.003 9-1.916 1.916z"></path></g></svg>
-                  <svg onClick={subtractScore} fill="#ffffff" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M16.003 18.626l7.081-7.081L25 13.46l-8.997 8.998-9.003-9 1.917-1.916z"></path></g></svg>
-                  <p>{message.score}</p>
+                  <svg className='cursor-pointer' onClick={addScore} fill="#ffffff" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M15.997 13.374l-7.081 7.081L7 18.54l8.997-8.998 9.003 9-1.916 1.916z"></path></g></svg>
+                  <svg className='cursor-pointer' onClick={subtractScore} fill="#ffffff" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M16.003 18.626l7.081-7.081L25 13.46l-8.997 8.998-9.003-9 1.917-1.916z"></path></g></svg>
+                  <p className=' select-none cursor-default'>{message.score}</p>
                 </div>
-                <p>Posted by {user.displayName}</p>
+                <p></p>
             </div>
             
         </div>
