9eea0b17ea8e93ff7a51d935e80701ee20eb724c
Max Petzold <maxpetzold581@gmail.com>
2023-05-10 14:05:14 +0200

    fikse navbar

--- Project/Schmatt/src/pages/forums/components/DisclosureMenu.jsx
+++ Project/Schmatt/src/pages/forums/components/DisclosureMenu.jsx
@@ -1,6 +1,7 @@
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
+import Codingnews from "./Coding-schmatts/Codingnews-shmub";
 
 export default function Discmenu({ selectedForum, setSelectedForum }) {
   return (
@@ -13,9 +14,8 @@ export default function Discmenu({ selectedForum, setSelectedForum }) {
               <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-2 text-left text-xl font-medium text-[#FFF8F8] #FFF8F8 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>Coding</span>
                 <ChevronUpIcon
-                  className={`${
-                    open ? "rotate-180 transform" : ""
-                  } h-5 w-5 text-[#FFF8F8]`}
+                  className={`${open ? "rotate-180 transform" : ""
+                    } h-5 w-5 text-[#FFF8F8]`}
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
@@ -28,7 +28,7 @@ export default function Discmenu({ selectedForum, setSelectedForum }) {
                 </a>
               </Disclosure.Panel>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500 active:bg-violet-500">
-                <a onClick={() => setSelectedForum("s/CodingNews")}>
+                <a onClick={() => { setSelectedForum("s/CodingNews"), <Codingnews /> }}>
                   s/CodingNews
                 </a>
               </Disclosure.Panel>
