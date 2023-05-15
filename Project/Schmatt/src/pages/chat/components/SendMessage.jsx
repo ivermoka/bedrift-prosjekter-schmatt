afc9b892f0e2067f8951cd2abab888011057e68b
ivermoka <ivermoka@icloud.com>
2023-05-15 08:28:37 +0200

    forum changes

--- Project/Schmatt/src/pages/chat/components/SendMessage.jsx
+++ Project/Schmatt/src/pages/chat/components/SendMessage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { useState } from "react";
-import { auth, db } from "../../../firebase-config";
+import { useState, useCallback } from "react";
+import { auth, db, storage } from "../../../firebase-config";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { useDropzone } from 'react-dropzone';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 const style = {
   form: `backdrop-blur-sm pt-[1px] h-14 w-[63%] flex text-xl absolute bottom-0 m-2 justify-between flex-row`,
   input: `w-[93%] backdrop-blur-sm opacity-75 pl-5 rounded-full text-l text-common p-3 bg-button-active text-white outline-none border-none`,
-  button: `p-3 bg-button-active rounded-full opacity-75`,
+  button: `p-3 bg-button-active rounded-full cursor-pointer opacity-75`,
 };
 
 const SendMessage = ({
@@ -17,6 +21,8 @@ const SendMessage = ({
   scrollRef,
 }) => {
   const [input, setInput] = useState("");
+  const [addImage, setAddImage] = useState(false);
+  const [imageURL, setImageURL] = useState("");
 
   const sendMessage = async (e) => {
     scrollRef.current.scroll({
@@ -41,6 +47,7 @@ const SendMessage = ({
       uid,
       timestamp: serverTimestamp(),
       room: selectedRoom,
+      image: imageURL
     });
     // ref.current.scrollIntoView({ behavior: "smooth" }); //funker ikke akk nå
     setInput("");
@@ -48,6 +55,7 @@ const SendMessage = ({
 
   return (
     <form onSubmit={sendMessage} className={style.form}>
+      <svg onClick={() => {setAddImage(true)}} className={style.button} fill="#626060" width="55px" height="55px" viewBox="-3.2 -3.2 38.40 38.40" xmlns="http://www.w3.org/2000/svg" transform="rotate(45)" stroke="#626060" stroke-width="0.45"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.064"></g><g id="SVGRepo_iconCarrier"><path d="M7.004 23.087l7.08-7.081-7.07-7.071L8.929 7.02l7.067 7.069L23.084 7l1.912 1.913-7.089 7.093 7.075 7.077-1.912 1.913-7.074-7.073L8.917 25z"></path></g></svg>
       <input
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -91,8 +99,80 @@ const SendMessage = ({
           </g>
         </svg>
       </button>
+      <Transition
+        appear
+        show={addImage}
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+        as={Fragment}
+      >
+        <Dialog
+          onClose={() => setAddImage(false)}
+          className="bg-state-blue opacity-90 fixed h-max-[20%] left-[22%] bottom-[8%] z-10 w-1/3 p-6 rounded-t-lg rounded-br-lg"
+        >
+          <Dialog.Panel className="flex flex-col gap-4 items-center">
+            <Dialog.Title className="text-3xl font-bold">Add image</Dialog.Title>
+            <MyDropzone setImageURL={setImageURL} />
+            <button
+              className="bg-delif-blue hover:bg-iris duration-300 p-2 rounded-lg w-1/2"
+              onClick={() => setAddImage(false)}
+              type="submit"
+            >
+              Got It!
+            </button>
+          </Dialog.Panel>
+        </Dialog>
+      </Transition>
     </form>
   );
 };
+function MyDropzone({setImageURL}) {
+  const [uploading, setUploading] = useState(false);
+
+  const onDrop = useCallback(async (acceptedFiles) => {
+    const file = acceptedFiles[0];
+    setUploading(true);
+
+    // Upload file to Firebase Storage
+    const storageRef = ref(storage, "images/" + file.name);
+    await uploadBytes(storageRef, file);
+
+    // Get download URL of uploaded file
+    const downloadURL = await getDownloadURL(storageRef);
+    setImageURL(downloadURL)
+
+    setUploading(false);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const refreshPage = () => {
+    window.location.reload;
+  };
+
+  return (
+    <>
+      <div
+        {...getRootProps()}
+        className="bg-white/[0.7] opacity-90 w-42 h-20 border-2 border-black border-dashed flex items-center text-center px-8"
+      >
+        <input {...getInputProps()} />
+        {uploading ? (
+          <p>Uploading...</p>
+        ) : isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      
+    </>
+  );
+}
+
 
 export default SendMessage;
--- Project/Schmatt/src/pages/forums/components/FormPopup.jsx
+++ Project/Schmatt/src/pages/forums/components/FormPopup.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../../../firebase-config";
+import { auth, db, storage } from "../../../firebase-config";
+import { useDropzone } from 'react-dropzone';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
 
 const FormPopup = ({ setCreateRoomPopup, selectedForum }) => {
     const [titleInput, setTitleInput] = useState("");
     const [contentInput, setContentInput] = useState("");
+    const [imageURL, setImageURL] = useState("");
     const forum = selectedForum.selectedForum;
     const score = 0;
 
@@ -30,7 +34,8 @@ const FormPopup = ({ setCreateRoomPopup, selectedForum }) => {
             uid,
             timestamp: serverTimestamp(),
             forum,
-            score
+            score,
+            image: imageURL
         });
     }
     function handleCreateRoomPopup() {
@@ -47,7 +52,7 @@ const FormPopup = ({ setCreateRoomPopup, selectedForum }) => {
                     placeholder="Your title"
                     className=' w-3/4 h-12 rounded-lg placeholder:text-black pl-2 bg-forum-backround bg-gray-700'
                 />
-
+                <MyDropzone setImageURL={setImageURL} />
                 <input
                     value={contentInput}
                     onChange={(e) => setContentInput(e.target.value)}
@@ -55,7 +60,7 @@ const FormPopup = ({ setCreateRoomPopup, selectedForum }) => {
                     placeholder="Your content"
                     className=' w-3/4 h-1/3 rounded-md placeholder:float-left placeholder:text-black text-start flex justify-start pl-2 bg-gray-500'
                 />
-                <button type="submit" className='bg-common rounded-lg p-2 w-full h-20 text-white'>
+                <button  type="submit" className='bg-common rounded-lg p-2 w-full h-20 text-white'>
                     Create post
                 </button>
             </form>
@@ -66,5 +71,48 @@ const FormPopup = ({ setCreateRoomPopup, selectedForum }) => {
         </div>
     )
 }
+function MyDropzone({setImageURL}) {
+    const [uploading, setUploading] = useState(false);
+  
+    const onDrop = useCallback(async (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      setUploading(true);
+  
+      // Upload file to Firebase Storage
+      const storageRef = ref(storage, "images/" + file.name);
+      await uploadBytes(storageRef, file);
+  
+      // Get download URL of uploaded file
+      const downloadURL = await getDownloadURL(storageRef);
+      setImageURL(downloadURL)
+  
+      setUploading(false);
+    }, []);
+  
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  
+    const refreshPage = () => {
+      window.location.reload;
+    };
+  
+    return (
+      <>
+        <div
+          {...getRootProps()}
+          className="bg-white/[0.7] opacity-90 w-42 h-20 border-2 border-black border-dashed flex items-center text-center px-8"
+        >
+          <input {...getInputProps()} />
+          {uploading ? (
+            <p>Uploading...</p>
+          ) : isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+        </div>
+        
+      </>
+    );
+  }
 
 export default FormPopup
