8889212238e6c93f465ad85633aca4ad4e251a68
ivermoka <ivermoka@icloud.com>
2023-05-24 09:24:41 +0200

    home finished

--- Project/Schmatt/src/pages/home/components/AdPage.jsx
+++ Project/Schmatt/src/pages/home/components/AdPage.jsx
@@ -10,15 +10,13 @@ const AdPage = () => {
 }
 function Box() {
     return (
-        <motion.div className=' w-screen h-screen flex z-[9] justify-center items-center gap-20' initial={{opacity:0, translateY:40}} whileInView={{opacity:1, translateY:0}} transition={{duration:0.5}} >
-            <div className=' w-1/3 h-2/3 border-border-color border'>
-                (gif of chat/forums)
+        <motion.div className=' w-screen h-screen flex z-[9] justify-center items-center gap-10' initial={{opacity:0, translateY:40}} whileInView={{opacity:1, translateY:0}} transition={{duration:0.5}} >
+            <div className=' w-3/5 h-3/5 border-border-color border'>
+                <img src='chatforum.gif' className=''></img>
             </div>
-            <div className=' w-1/3 pl-10  lg:text-[2.5rem] md:text-[1rem] sm:text-[0.5rem] pt-10 h-2/3 text-[1rem] text-text2  border-border-color border'>
-                <p>
-                    Introducing our innovative and user-friendly platform 
-                    that seamlessly connects you to the world of digital content and creativity.
-                </p>
+            <div className=' w-1/3 pl-10  lg:text-[2rem] md:text-[1rem] sm:text-[0.5rem] pt-10 h-3/5 text-[1rem] text-text-color  border-border-color border'>
+                Introducing our innovative and user-friendly platform 
+                that seamlessly connects you to the world of digital content and creativity.
             </div>
         </motion.div>
     )
--- Project/Schmatt/src/pages/home/components/GetStartedPage.jsx
+++ Project/Schmatt/src/pages/home/components/GetStartedPage.jsx
@@ -22,7 +22,7 @@ function Box() {
       <motion.div className='  backdrop-blur-sm w-screen h-screen items-center flex z-[8] ' initial={{opacity:0, translateY:10}} whileInView={{opacity:1, translateY:0}} transition={{duration:1}} >
         <div className=' pt-40 z-10 h-[110vh] w-screen  flex flex-col items-center text-text-color'>
           <p className='select-none cursor-default text-[4.5rem]'>Easy and free!</p>
-          <p className='select-none cursor-default text-[1rem'>Get chatting with the click of a button!</p>
+          <p className='select-none cursor-default text-[1rem'>Get chatting and posting with the click of a button!</p>
           <Link href="/signup">
             <button on className='mt-12 text-3xl bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-700 hover:border-transparent rounded'>
               Sign up
@@ -44,11 +44,41 @@ function Box2() {
   return (
       <motion.div className='backdrop-blur-sm pt-10 z-10 h-[110vh] w-screen flex flex-col items-center text-text-color' initial={{opacity:0, translateY:10}} whileInView={{opacity:1, translateY:0}} transition={{duration:2}} >
         <div className='flex flex-row z-10 h-full w-full px-32 mb-20 flex-wrap '>
-          <div className='basis-1/2 pr-5 flex justify-end'>Chat</div>
-          <div className='basis-1/2 bg-slate-600'>(img)</div>
-          <div className='basis-1/2 bg-slate-600 '>(img)</div>
-          <div className='basis-1/2 pl-5 pt-1'>Forums</div>
+          <div className='p-2 basis-1/2 flex justify-around items-end flex-col'>
+            <div className='flex flex-col items-end'>
+              <p className='font-bold text-xl'>Chat</p>
+              
+              <p>Chat with your friends and family</p>
+            </div>
+            <a href='/chat'>
+              <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-700 hover:border-transparent rounded text-xl'>Go to chat</button>
+            </a>
+            
+          </div>
+          <div className='basis-1/2 '>
+            <img src='chat.png'></img>
+          </div>
+          <div className='basis-1/2  '>
+            <img src='forums.png'></img>
+          </div>
+          <div className='p-2 basis-1/2 flex justify-around items-start flex-col'>
+            <div>
+              <p className='font-bold text-xl'>Forums</p>
+              
+              <p>Join forums and discuss your interests</p>
+            </div>
+            <a href='/forums'>
+              <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-700 hover:border-transparent rounded text-xl'>Go to forums</button>
+            </a>
+            
+          </div>
+          <footer className='flex flex-col items-center justify-center w-full h-20 pb-20 pt-40 text-text-color'>
+            <p className='text-xl'>Schmatt</p>
+            <p className='text-sm'>© 2023 Schmatt. All rights reserved.</p>
+
+          </footer>
         </div>
+        
         
       </motion.div>
   )
